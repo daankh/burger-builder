ee39f6a62a5252b1e4d9d6df93cd961d9c757921
daankh <jarosz.dan@gmail.com>
2020-10-07 17:41:56 +0200

    feat(ContactData): add and style

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
+import { Route } from "react-router-dom";
+import ContactData from "./ContactData/ContactData";
 
 class Checkout extends Component {
   state = {
@@ -40,6 +42,10 @@ class Checkout extends Component {
           ingredients={ingredients}
           checkoutContinued={this.checkoutContinuedHandler}
           checkoutCancelled={this.checkoutCancelledHandler}
+        />
+        <Route
+          path={`${this.props.match.path}/contact-data`}
+          component={ContactData}
         />
       </div>
     );
