d9784d39896719a5c0861cfc8ada7d3e57d57f03
daankh <jarosz.dan@gmail.com>
2020-10-21 21:07:08 +0200

    feat: prepare structure of order store

---
+++ src/redux/reducers/order.js
@@ -0,0 +1,11 @@
+const initialState = {}
+
+const orderReducer = (state = initialState, action) => {
+  const {type} = action;
+  switch (type) {
+    default:
+      return state
+  }
+}
+
+export default orderReducer
--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import burgerBuilder from "../reducers/burgerBuilder";
+import burgerBuilderReducer from "../reducers/burgerBuilder";
+import orderReducer from "../reducers/order";
 
 //it allows us to apply middleware and use redux dev tools in the same time
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -8,7 +9,8 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
   const store = createStore(
     combineReducers({
-      burgerBuilder: burgerBuilder,
+      burgerBuilder: burgerBuilderReducer,
+      order: orderReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
